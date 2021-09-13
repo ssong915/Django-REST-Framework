ac7b389c87ad6e6f104939473f58b894702254a8
ssong <yysk_915@naver.com>
2021-09-13 17:35:14 +0900

    [Add]: 3.Class-based Views

--- DRF_tutorial/snippets/urls.py
+++ DRF_tutorial/snippets/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # path('snippets/', views.snippet_list),
     # path('snippets/<int:pk>', views.snippet_detail),
 
-    ## 클래스기반
+    ## 클래스기반 
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
