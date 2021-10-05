510695310e3807f92bea4063b1aa2a5d9569c869
ssong <yysk_915@naver.com>
2021-10-06 00:45:25 +0900

    [Add] 게시판 글쓰기, 목록 재정의(작성자 추가)

--- DRF_Board/project/serializers.py
+++ DRF_Board/project/serializers.py
@@ -2,6 +2,8 @@ from .models import Blog
 from rest_framework import serializers
 
 class BlogSerializer(serializers.ModelSerializer):
+    # view에서 넘긴 user 를 받아와서 user.nickname -> 작성자
+    user = serializers.ReadOnlyField(source = 'user.nickname')
     class Meta:
         model = Blog
         fields = '__all__' 
