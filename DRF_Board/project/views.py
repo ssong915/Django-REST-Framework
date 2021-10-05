d0b26792b14af78b69df699e46f9264c09fba2ae
ssong <yysk_915@naver.com>
2021-10-06 00:49:25 +0900

    [Add] 비로그인 readonly

--- DRF_Board/project/views.py
+++ DRF_Board/project/views.py
@@ -1,9 +1,17 @@
 from .models import Blog
 from .serializers import BlogSerializer
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Blog의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class BlogViewSet(viewsets.ModelViewSet):
+    # authentication 추가
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+    # permission 추가 
+    # IsAuthenticatedOrReadOnly : 비회원은 글을 볼 수만 있어야 하기 때문!
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
    
